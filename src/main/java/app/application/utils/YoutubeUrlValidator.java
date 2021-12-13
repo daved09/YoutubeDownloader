05526fc5f991f2594b33c303f2733a23754fece0
dbannasch <David.Bannasch@abas.de>
2021-12-13 15:54:14 +0100

    shortlinks von youtubes share funktion werden nun supported

--- src/main/java/app/application/utils/YoutubeUrlValidator.java
+++ src/main/java/app/application/utils/YoutubeUrlValidator.java
@@ -8,16 +8,20 @@ public class YoutubeUrlValidator {
 
     private final UrlValidator urlValidator = new UrlValidator();
 
-    public boolean isYoutubeUrlInvalid(String url){
-        return !urlValidator.isValid(url) || !isYoutubeUrl(url) || !hasVideoOrPlaylistID(url);
+    public boolean isYoutubeUrlValid(String url){
+        return urlValidator.isValid(url) || isYoutubeUrl(url) || hasVideoOrPlaylistID(url);
+    }
+
+    public boolean isShortUrl(String url){
+        return url.contains("youtu.be");
     }
 
     private boolean isYoutubeUrl(String url){
-        return url.contains("youtube.com");
+        return url.contains("youtube.com") || isShortUrl(url);
     }
 
     private boolean hasVideoOrPlaylistID(String url){
-        return url.contains("v=") || url.contains("list=");
+        return url.contains("v=") || url.contains("list=") || isShortUrl(url);
     }
 
 }
