b9ffbb3497c5e8cd82ab251f1a53aebc3c5500df
Dave <David.Bannasch@mailbox.org>
2022-03-23 18:50:45 +0100

    last class converted to kotlin

--- src/main/java/app/MyApp.kt
+++ src/main/java/app/MyApp.kt
@@ -1,15 +1,11 @@
-package app;
+package app
 
-
-import app.application.YoutubeDownloader;
-import javafx.application.Application;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import app.application.YoutubeDownloader
+import javafx.application.Application
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-public class MyApp {
-
-    public static void main(String[] args) {
-        Application.launch(YoutubeDownloader.class, args);
+open class MyApp
+    fun main(args: Array<String>) {
+        Application.launch(YoutubeDownloader::class.java, *args)
     }
-
-}
