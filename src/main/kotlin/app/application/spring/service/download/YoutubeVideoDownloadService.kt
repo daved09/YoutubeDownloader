b52d0548a9e208ab437cce352d66b8302c7fee8a
daved09 <17879596+daved09@users.noreply.github.com>
2022-11-12 12:09:11 +0100

    reverted the deletion for single video download

--- src/main/kotlin/app/application/spring/service/download/YoutubeVideoDownloadService.kt
+++ src/main/kotlin/app/application/spring/service/download/YoutubeVideoDownloadService.kt
@@ -2,35 +2,41 @@ package app.application.spring.service.download
 
 import app.application.data.entities.YoutubeVideo
 import app.application.data.entities.YoutubeVideoSettingsEntity
+import app.application.exception.CantDeleteFileException
 import com.github.kiulian.downloader.downloader.request.RequestVideoFileDownload
 import com.github.kiulian.downloader.model.videos.formats.AudioFormat
 import com.github.kiulian.downloader.model.videos.formats.Format
 import lombok.SneakyThrows
 import org.springframework.stereotype.Service
+import java.io.File
 import java.nio.file.Paths
 
 @Service
 class YoutubeVideoDownloadService : YoutubeDownloadService() {
     @SneakyThrows
     fun downloadVideoAsync(youtubeVideoSettingsEntity: YoutubeVideoSettingsEntity) {
-        downloadAsync(
-            youtubeVideoSettingsEntity.youtubeEntity!!,
-            if (youtubeVideoSettingsEntity.settingsEntity?.audioOnlyProperty?.get()!!)
-                getAudioFormat(youtubeVideoSettingsEntity) else youtubeVideoSettingsEntity.settingsEntity?.qualityProperty?.get())
+        downloadAsync(youtubeVideoSettingsEntity)
     }
 
     @SneakyThrows
-    protected fun downloadAsync(youtubeVideo: YoutubeVideo, format: Format?) {
-        val requestVideoFileDownload = RequestVideoFileDownload(format)
+    protected fun downloadAsync(youtubeVideoSettingsEntity: YoutubeVideoSettingsEntity) {
+        val requestVideoFileDownload = RequestVideoFileDownload(youtubeVideoSettingsEntity.settingsEntity?.qualityProperty?.get())
         requestVideoFileDownload.callback(youtubeDownloadListener)
-                .renameTo(youtubeVideo.videoTitle)
+                .renameTo(youtubeVideoSettingsEntity.youtubeEntity?.videoTitle)
                 .overwriteIfExists(userConfigHandler.userConfig!!.overwriteExistingVideo.get())
                 .saveTo(Paths.get(userConfigHandler.userConfig!!.downloadDir.get()).toFile())
-        youtubeDownloader.downloadVideoFile(requestVideoFileDownload).data()
+        val videoFile = youtubeDownloader.downloadVideoFile(requestVideoFileDownload).data()
+        if(youtubeVideoSettingsEntity.settingsEntity?.audioOnlyProperty?.get()!!){
+            youtubeVideoConverter.convert(videoFile)
+            deleteOldVideoFile(videoFile)
+        }
     }
 
-    private fun getAudioFormat(youtubeVideoSettingsEntity: YoutubeVideoSettingsEntity): AudioFormat {
-        return youtubeVideoSettingsEntity.youtubeEntity!!.audioFormat
+    private fun deleteOldVideoFile(videoFile: File){
+        val success = videoFile.delete()
+        if(!success){
+            throw CantDeleteFileException("File ${videoFile.absoluteFile} couldn´t be deleted.")
+        }
     }
 
 
