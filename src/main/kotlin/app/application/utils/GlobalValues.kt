47021928264acaaa651a6fd52631be32e6effaec
Daved09 <17879596+daved09@users.noreply.github.com>
2023-08-04 12:43:17 +0200

    comments removed

--- src/main/kotlin/app/application/utils/GlobalValues.kt
+++ src/main/kotlin/app/application/utils/GlobalValues.kt
@@ -7,7 +7,7 @@ class GlobalValues private constructor() {
     }
 
     companion object {
-        //Downloader Spezifisch
+
         const val VIDEO_DIRECTORY = "Videos"
         const val VIDEO_DIRECTORY_MAC = "Movies"
         const val YOUTUBE_URL = "youtube.com"
@@ -15,10 +15,8 @@ class GlobalValues private constructor() {
         const val DOWNLOADER_CONFIG_FILENAME = ".ytdl.json"
         const val DOWNLOAD_PAGE = "https://github.com/daved09/YoutubeDownloader/releases/latest"
 
-        //Exceptions
         const val DOWNLOAD_EXECUTOR_TERMINATION_ERROR = "Termination of download executor was not Successful"
 
-        //Java Generell
         const val USER_HOME = "user.home"
     }
 }
